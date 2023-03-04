0b57ba94baff944246fa86ae00e9a33bf6d330d5
xchristx <christian.aguilar.dev@gmail.com>
2023-03-04 13:32:37 -0400

    restableciendo configuracion de git

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -28,17 +28,13 @@ export default function Page404() {
       <Container>
         <StyledContent sx={{ textAlign: 'center', alignItems: 'center' }}>
           <Typography variant="h3" paragraph>
-            Sorry, page not found!
-          </Typography>
-
-          <Typography sx={{ color: 'text.secondary' }}>
-            Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve mistyped the URL? Be sure to check your spelling.
+            Lo sentimos, página no encontrada
           </Typography>
 
           <Box component="img" src="/assets/illustrations/illustration_404.svg" sx={{ height: 260, mx: 'auto', my: { xs: 5, sm: 10 } }} />
 
           <Button to="/dashboard/usuario" size="large" variant="contained" component={RouterLink}>
-            Go to Home
+            Regresa a inicio
           </Button>
         </StyledContent>
       </Container>
