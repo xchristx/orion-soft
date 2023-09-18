4d9d896c4384a7b36ae0e44f0afcdccd17a5c0ee
xchristx <christian.aguilar.dev@gmail.com>
2023-09-18 13:43:27 -0400

    fireb config

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export const FIREBASE_API = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_authDomain,
-  databaseURL: import.meta.env.VITE_databaseURL,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
-  measurementId: import.meta.env.VITE_measurementId,
+  apiKey: 'AIzaSyB9EunLbLYlOCbveWiSVVmvxdbQXm6SlXI',
+  authDomain: 'orion-soft.firebaseapp.com',
+  databaseURL: 'https://orion-soft-default-rtdb.firebaseio.com',
+  projectId: 'orion-soft',
+  storageBucket: 'orion-soft.appspot.com',
+  messagingSenderId: '890826928910',
+  appId: '1:890826928910:web:bca1bd472541b2d362dd5f',
+  measurementId: 'G-HX3425DC19',
 };
